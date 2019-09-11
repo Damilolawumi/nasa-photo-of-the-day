064f37d73e6e8b3ce108ca980c51b73ef398e2ce
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-11 19:21:17 +0100

    commented out the function for api call and my custome

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
 
   const [nasaInfo, setNasaInfo] = useState({})
 
-//   useEffect(() => {
-//     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-//     .then(response => {
-//       console.log(response)
+  //   useEffect(() => {
+  //     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //     .then(response => {
+  //       console.log(response)
 
-//       setNasaInfo(response.data)
-//     })
-     
-// }, [])
+  //       setNasaInfo(response.data)
+  //     })
+
+  // }, [])
 
 
 
@@ -28,10 +28,24 @@ function App() {
         WELCOME TO THE NASA WORLD
     </h1>
 
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {/* <Name title={nasaInfo.title} explanation={nasaInfo.explanation} />
+      <Photo imgUrl={nasaInfo.url} /> */}
+
+      <Name
+        title={"IC 1805: The Heart Nebula"}
+        explanation={`What energizes the Heart Nebula? First, the large emission nebula dubbed IC 1805 looks, 
+      in whole, like a human heart.  The nebula glows brightly in red light emitted by its most prominent element: 
+      hydrogen.  The red glow and the larger shape are all powered by a small group of stars near the nebula's center. 
+       In the center of the Heart Nebula are young stars from the open star cluster Melotte 15 that are
+        eroding away several picturesque dust pillars with their energetic light and winds.
+         The open cluster of stars contains a few bright stars nearly 50 times the mass of our Sun,
+          many dim stars only a fraction of the mass of our Sun, and an absent microquasar that was 
+          expelled millions of years ago.  The Heart Nebula is located about 7,500 light years away toward 
+          the constellation of Cassiopeia.  Coincidentally, a small meteor was captured in the foreground 
+          during imaging  and is visible above the dust pillars. 
+           At the top right is the companion Fishhead Nebula.`}
+      />
+      <Photo imgUrl={"https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg"} />
     </div>
   );
 }
