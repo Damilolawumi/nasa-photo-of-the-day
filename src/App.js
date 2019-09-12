a6fdbafdc8bff62dea03c7f9ab8c3bea7aa0c2c2
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-12 18:20:38 +0100

    styled the paragraph

--- src/App.js
+++ src/App.js
@@ -3,36 +3,39 @@ import Photo from './Photo'
 import Name from './Name'
 import "./App.css";
 import axios from "axios"
-import styled from 'styled-components'
+import styled from "styled-components"
+
+const ContainerDiv = styled.div`
+text-align: center;
+`;
 
 function App() {
 
   const [nasaInfo, setNasaInfo] = useState({})
 
-  //   useEffect(() => {
-  //     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  //     .then(response => {
-  //       console.log(response)
+    useEffect(() => {
+       axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        console.log(response)
 
-  //       setNasaInfo(response.data)
-  //     })
+        setNasaInfo(response.data)
+      })
 
-  // }, [])
-
-
-
+  }, [])
 
 
   return (
-    <div className="App">
+    <ContainerDiv className="App">
       <h1>
         PHOTO OF THE DAY
     </h1>
 
       <Name title={nasaInfo.title} explanation={nasaInfo.explanation} />
       <Photo imgUrl={nasaInfo.url} />
-    </div>
+    </ContainerDiv>
   );
+
+
 }
 
 export default App;
